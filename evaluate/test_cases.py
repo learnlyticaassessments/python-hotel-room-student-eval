ab5e257eeeaf84e025cde9aa1e1213a48beab7e5
learnlyticaassessments <assessments@learnlytica.com>
2025-03-24 16:25:12 +0530

    Update test_cases.py

--- evaluate/test_cases.py
+++ evaluate/test_cases.py
@@ -1,23 +1,21 @@
-from student import Room, LuxuryRoom
-
-def test_room_str():
-    r = Room("Alice")
+def test_room_str(student):
+    r = student.Room("Alice")
     return str(r) == "Room booked for Alice"
 
-def test_luxury_str():
-    lr = LuxuryRoom("Bob", 5)
+def test_luxury_str(student):
+    lr = student.LuxuryRoom("Bob", 5)
     return str(lr) == "Luxury Room booked for Bob with 5 minibar items"
 
-def test_del():
+def test_del(student):
     try:
-        r = Room("Charlie")
+        r = student.Room("Charlie")
         del r
         return True
     except:
         return False
 
-def test_subclass_check():
-    return issubclass(LuxuryRoom, Room)
+def test_subclass_check(student):
+    return issubclass(student.LuxuryRoom, student.Room)
 
 test_suite = {
     "TC1": (test_room_str, 2.5),
